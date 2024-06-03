1b34ae25930a83d76a19a4ef264acf3df6ef5595
yoruck2 <yoruck2@gmail.com>
2024-06-04 05:16:22 +0900

    TalkListViewController 추가

--- TravelTalk/TalkList/TalkListViewController.swift
+++ TravelTalk/TalkList/TalkListViewController.swift
@@ -7,13 +7,94 @@
 
 import UIKit
 
-class TalkListViewController: UIViewController {
 
+
+class TalkListViewController: UIViewController {
+    
+    @IBOutlet var talkListTableView: UITableView!
+    
+    var searchedUserList: [User] = User.allCases {
+        didSet {
+            talkListTableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "TRAVEL TALK"
+        configureTableView()
+        configureSearchBar()
     }
-
-
+    
+    func configureTableView() {
+        talkListTableView.delegate = self
+        talkListTableView.dataSource = self
+        registerCell(to: talkListTableView, cellId: TalkListTableViewCell.identifier)
+        talkListTableView.separatorStyle = .none
+    }
 }
 
+extension TalkListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering {
+            searchedUserList.count
+        } else {
+            User.allCases.count - 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let data = searchedUserList[indexPath.row]
+        
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            let vc = sb.instantiateViewController(withIdentifier: TalkRoomViewController.identifier) as! TalkRoomViewController
+            
+//            vc.cityInfoData = data
+            
+            navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let userData = searchedUserList[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: TalkListTableViewCell.identifier, for: indexPath) as! TalkListTableViewCell
+        
+        cell.data = userData
+        cell.setUpData()
+        return cell
+    }
+}
+
+extension TalkListViewController: UISearchResultsUpdating {
+    
+    // MARK: - 서치바 설정
+    func configureSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
+    }
+    
+    var isFiltering: Bool {
+        let searchController = self.navigationItem.searchController
+        let isActive = searchController?.isActive ?? false
+        let isSearchBarHasText = searchController?.searchBar.text?.isEmpty == false
+        return isActive && isSearchBarHasText
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text?.lowercased() else {
+            searchedUserList = User.allCases
+            return
+        }
+        
+        if text == "" {
+            searchedUserList = User.allCases
+            
+        } else {
+            searchedUserList = User.allCases.filter {
+                $0.rawValue.lowercased().contains(text)
+            }
+        }
+    }
+}

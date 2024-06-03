e6700332c5a00dbd78ec2ee102a9677f3c666857
yoruck2 <yoruck2@gmail.com>
2024-06-04 05:15:50 +0900

    ReusableProtocol 추가

--- TravelTalk/ReusableProtocol.swift
+++ TravelTalk/ReusableProtocol.swift
@@ -5,4 +5,26 @@
 //  Created by dopamint on 6/4/24.
 //
 
-import Foundation
+import UIKit
+
+protocol ReusableProtocol: AnyObject {
+    static var identifier: String { get }
+    
+}
+
+extension UITableViewCell: ReusableProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UIViewController: ReusableProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    func registerCell(to tableView: UITableView, cellId: String) {
+        let nib = UINib(nibName: cellId, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: cellId)
+    }
+}

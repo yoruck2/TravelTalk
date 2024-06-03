29b2eb9552889e8f4dec37b6ded74658365fac1d
yoruck2 <yoruck2@gmail.com>
2024-06-04 05:16:06 +0900

    TalkListTableViewCell 추가

--- TravelTalk/TalkList/TalkListTableViewCell.swift
+++ TravelTalk/TalkList/TalkListTableViewCell.swift
@@ -9,15 +9,40 @@ import UIKit
 
 class TalkListTableViewCell: UITableViewCell {
 
+    var data: User?
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var talkRoomTitleLabel: UILabel!
+    @IBOutlet var recentMessageLabel: UILabel!
+    @IBOutlet var recentMessageDateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        configureLayout()
     }
     
+//    override func layoutSubviews() {
+//        setUpData()
+//    }
+    
+    func configureLayout() {
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+        profileImageView.clipsToBounds = true
+        talkRoomTitleLabel.font = .boldSystemFont(ofSize: 15)
+        
+        recentMessageLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+        recentMessageLabel.textColor = #colorLiteral(red: 0.4661759734, green: 0.4578384757, blue: 0.5037643909, alpha: 1)
+        recentMessageDateLabel.font = .systemFont(ofSize: 14)
+        recentMessageDateLabel.textColor = #colorLiteral(red: 0.5764706731, green: 0.5764706731, blue: 0.5764706731, alpha: 1)
+        
+    }
+    
+    func setUpData() {
+        guard let data else {
+            return
+        }
+        profileImageView.image = UIImage(named: data.profileImage)
+        talkRoomTitleLabel.text = data.rawValue
+        recentMessageLabel.text = data.rawValue
+        recentMessageDateLabel.text = data.rawValue
+    }
 }
